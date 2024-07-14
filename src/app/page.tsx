1137c709a4c19bc3a6b969f277f66d36e835a4e1
Swank <willmadedesign@gmail.com>
2024-07-14 16:16:55 -0700

    Feat: Add siteConfig + header structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello, website under construction :) </div>
-    </main>
+    <section>Hello Mom :) this site is under construction!</section>
   );
 }
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Willmade",
+    url: "https://example.com",
+    description: "Your Will, Made.",
+    author: "SwankSkywalker",
+    links: {
+        twitter: "https://x.com/SwankSauceSays",
+        github: "https://github.com/SwankSkywalker",
+        radio: "https://soundcloud.com/willmadesound",
+    },
+}
+
+export type SiteConfig = typeof siteConfig
