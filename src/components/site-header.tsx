0e2c174e500ad067bf755157f9af478ed509f435
Swank <willmadedesign@gmail.com>
2024-07-18 18:17:44 -0700

    Chore: Fix nav width

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,7 +6,7 @@ import { Icons } from "./icons";
 
 export function SiteHeader() {
     return (
-        <header className="z-10 sticky top-0 w-4 border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-14 max-w-screen-2xl items-center">
                 <div className="flex flex-1 items-center justify-end space-x-2">
                     <nav className="flex items-center">
