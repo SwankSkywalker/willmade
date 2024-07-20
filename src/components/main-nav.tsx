5a745351e6e7301eb7c9229768f70185de39ce02
Swank <willmadedesign@gmail.com>
2024-07-19 19:13:15 -0700

    Feat: Main nav layout w/ pages

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,5 +1,35 @@
-"use client"
+"use client";
+
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export function MainNav() {
-    return <nav className="flex items-center space-x-4 lg:space-x-6"></nav>
+    const pathName = usePathname();
+    return (
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+            <Link href="/" className="mr-6 flex items-center space-x-2">
+                <span className="font-bold italic">{siteConfig.name}</span>
+            </Link>
+            <Link href="/words" className={cn(
+                "text-sm font-semibold italic transition-colors hover:text-primary",
+                pathName === "/words" ? "text-foreground" : "text-foreground/60"
+                )}>
+                    Words
+            </Link>
+            <Link href="/moments" className={cn(
+                "text-sm font-semibold italic transition-colors hover:text-primary",
+                pathName === "/words" ? "text-foreground" : "text-foreground/60"
+                )}>
+                    Moments
+            </Link>
+            <Link href="/Radio" className={cn(
+                "text-sm font-semibold italic transition-colors hover:text-primary",
+                pathName === "/words" ? "text-foreground" : "text-foreground/60"
+                )}>
+                    Radio
+            </Link>
+        </nav>
+    );
 }
