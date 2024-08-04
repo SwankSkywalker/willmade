a2009de58745863e4c07d87fbb784488fd2435d6
Swank <willmadedesign@gmail.com>
2024-08-03 23:35:32 -0700

    Feat: begin adding blog page structure

---
+++ src/app/words/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default async function WordsPage() {
+    return (
+        <div className="container max-w-4xl py-6 lg:py-10">
+            <div className="flex flex-col item-start gap-4 md:flex-row md:justify-between md:gap-8">
+                <div className="flex-1 space-y-4">
+                    <h1 className="inline-block font-black italic text-4xl lg:tex-5xl">Words</h1>
+                <p className="text-xl text-muted-foreground">
+                    Getting out of my head, and getting it online.
+                </p>
+                </div>
+            </div>
+            <hr className="mt-8" />
+        </div>
+    )
+}
