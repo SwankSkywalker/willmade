5e739427d704b16266bdd85e458470bac55c076e
Swank <willmadedesign@gmail.com>
2024-08-13 17:58:54 -0700

    Feat: setup blog page + render our MDX

--- src/app/words/[...slug]/page.tsx
+++ src/app/words/[...slug]/page.tsx
@@ -1,4 +1,5 @@
 import { posts } from "#site/content";
+import { MDXContent } from "@/components/mdx-components";
 import { notFound } from "next/navigation";
 
 interface PostPageProps {
@@ -30,6 +31,9 @@ export default async function PostPage({params}: PostPageProps) {
     return (
         <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
             <h1 className="mb-2">{post.title}</h1>
+            {post.description ? (<p className="text-xl mt-0 text-muted-foreground">{post.description}</p>): null}
+            <hr className="my-4" />
+            <MDXContent code={post.body} />
         </article>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss"
 
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config = {
   darkMode: ["class"],
@@ -79,7 +79,10 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"), 
+    require("@tailwindcss/typography"),
+  ],
 } satisfies Config
 
 export default config
