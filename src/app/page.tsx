02827df0cc54a9e5e18e40a50708c9a5c7f6a3b1
Devin Donovan <willmadedesign@gmail.com>
2025-02-04 22:18:01 -0800

    Chore: Change layout to grid + center content on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
 
       {/* Main container for rest of page */}
       <div className="container max-w-4xl py-6 lg:py-10">
-        <div className="flex items-center gap-4 md:flex-row md:justify-between md:gap-8">
-          <div className="p-4 mx-auto content-center">Hello Mom :) this site is under construction!</div>
+        <div className="grid place-items-center h-screen">
+          <p className="p-4 mx-auto content-center">Hello Mom :) this site is under construction!</p>
         </div>
       </div>
     </>
