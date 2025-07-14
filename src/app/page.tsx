ee972494cfe1567951699c0c833322430f2bc37f
Devin Donovan <willmadedesign@gmail.com>
2025-07-13 22:35:25 -0700

    Chore: Updates landing page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,14 @@ export default function Home() {
 
       {/* Main container for rest of page */}
       <div className="container max-w-4xl py-6 lg:py-10">
-        <div className="grid h-screen">
+        <div className="grid h-svh">
           <p className="p-1 mx-auto place-content-center text-6xl">
             <b><em>Salut!</em></b>< br/>
           </p>
-          <p className="p-4 mx-auto content-center">
+          <p className="p-4 mx-auto content-center h-0">
             Welcome to my little corner of the internet< br/>
-            A smorgasbord of creativity, curiosity, and chaos.
+            A smorgasbord of creativity, curiosity, and chaos.< br/>
+            And as more gets added this section will expand out.
           </p>
         </div>
       </div>
