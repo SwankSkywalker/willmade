2959a0292da0394bdd96dace82706e8780296b34
Devin Donovan <willmadedesign@gmail.com>
2026-08-20 15:47:38 -0700

    Fix: Updates pkgs + turboPack migration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { build } from 'velite'
 
 /** @type {import('next').NextConfig} */
 export default {
+  turbopack: {},
   // othor next config here...
   webpack: config => {
     config.plugins.push(new VeliteWebpackPlugin())
